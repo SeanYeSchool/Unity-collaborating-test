d02bea405c35b82110d7137b82b00253fe5e0fd9
Sean-Ye-Student <86925941+SeanYeSchool@users.noreply.github.com>
2021-10-17 11:45:35 -0400

    Added mad guy

--- Team Test/Assets/Scripts/Movement.cs
+++ Team Test/Assets/Scripts/Movement.cs
@@ -8,34 +8,83 @@ public class Movement : MonoBehaviour
     public GameObject go;
     public float speed;
     private  Rigidbody2D rb;
+    private Transform my_transform;
+    public Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = go.GetComponent<Rigidbody2D>();
+        my_transform = go.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (target != null)
         {
-            rb.velocity = new Vector2(rb.velocity.x, speed);
-        }
+            Vector2 pos = my_transform.position;
+            Vector2 pos2 = target.position;
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }
+            float x_diff = pos2.x - pos.x;
+            float y_diff = pos2.y - pos.y;
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, -speed);
-        }
+            if (pos2.x < pos.x)
+            {
+                x_diff = -1;
+            }
+            else 
+            {
+                if (pos2.x > pos.x)
+                {
+                    x_diff = 1;
+                }
+                else 
+                {
+                    x_diff = 0;
+                }
+            }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if (pos2.y < pos.y)
+            {
+                y_diff = -1;
+            }
+            else
+            {
+                if (pos2.y > pos.y)
+                {
+                    y_diff = 1;
+                }
+                else
+                {
+                    y_diff = 0;
+                }
+            }
+
+            rb.velocity = new Vector3(x_diff * speed, y_diff * speed);
         }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -speed);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+        }
+        
     }
 }
